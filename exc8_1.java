9b65e64968238d125248e24f31d006f748e9af6c
MRxPosiTiVe <100093644+MRxPosiTiVe@users.noreply.github.com>
2023-04-26 22:34:43 +0500

    Add files via upload

--- exc8_1.java
+++ exc8_1.java
@@ -2,34 +2,41 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class exc8_1 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> arr = new ArrayList<>();
-        ArrayList<Integer> multp = new ArrayList<>();
         int num, len, smt = 0, ans = 0;
         System.out.println("Введите кол-во знаков числа в 3-ой СС: ");
         len = input.nextInt();
 
         while (smt != len) {
-            System.out.println("Введите число в 3-ой СС (т.е. числа: 0, 1, 2): ");
+            System.out.print("Введите число в 3-ой СС (т.е. числа: 0, 1, 2): ");
             num = input.nextInt();
-            if (num >= 0 & num <= 2) {
+            if (num >= 0 && num <= 2) {
                 arr.add(num);
                 smt++;
-            } else
+            } else {
                 System.out.println("Числа которые могут быть (0, 1, 2)");
-        }
-        for (int i = len - 1; i >= 0; i--) {
-            int mult = (int) (Math.pow(3, i));
-            multp.add(mult);
-            }
-        for (int i = 0; i <= len - 1; i++) {
-            for (int j = 0; j <= len - 1; j++) {
-                ans += arr.get(i) * multp.get(j);
-                i++;
             }
         }
-        System.out.println(arr);
-        System.out.println(ans);
+
+        System.out.print("Массив: ");
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr.size(); i++) {
+            int pow = len - 1 - i;
+            int digit = arr.get(i);
+            if (digit < 0 || digit > 2) {
+                System.out.println("Введено некорректное значение.");
+                return;
+            }
+            ans += digit * Math.pow(3, pow);
+        }
+
+        System.out.println("Результат перевода: " + ans);
     }
 }
+
